0c91c58547196f10bf924e377ebc9498a81dcb2a
Cargill Seiveright <microcargill@hotmail.com>
2020-02-14 13:25:34 -0500

    Some changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,11 @@
 from flask import Flask
+from flask_mail import Mail
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '}5[nY*(3%#nknmh"5K3X[+]LCj2g*Z#%^a<ez#U{'
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
+app.config['MAIL_PORT'] = '465' # (or try 2525)
+app.config['MAIL_USERNAME'] = 'fb4f67bdb72464'
+app.config['MAIL_PASSWORD'] = '07bf8323576841'
+mail = Mail(app)
 from app import views
